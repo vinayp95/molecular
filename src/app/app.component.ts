5f42b68f4a63a237437ddce53ee04dd3ae2b5244
Vinay Pattan <vinay.r@webnish.com>
2020-10-19 22:08:30 +0530

    vin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,56 @@ import { MoleculeService } from './molecule.service';
 })
 export class AppComponent implements OnInit {
   title = 'TreeView';
-  data = {}
+  data = {};
+
+  parentItems = [];
+
   constructor(private moleculeservice: MoleculeService) { }
 
   ngOnInit() {
     this.moleculeservice.getData(1).subscribe((res: any) => {
-      this.data['name'] = res.name
+      this.data['name'] = res.name;
       this.data['items'] = [];
       res.termsrelation.forEach(el => {
-        this.data['items'].push({ name: el.rel, children: el.terms, level: 1, loaded: true })
+        this.data['items'].push({ name: el.rel, children: [], level: 1, loaded: true });
+        this.parentItems.push({ name: el.rel, children: el.terms, level: 1, loaded: true });
       });
     });
   }
 
-  getChildren(child) {
-    if (!(child)) {
-      child = {
-        items: []
+  getParent(parent) {
+    for (let i = 0; i < this.parentItems.length; i++) {
+      if (this.parentItems[i].name === parent.name) {
+        if (this.data['items'][i].children.length === 0) {
+          this.data['items'][i].children = this.parentItems[i].children;
+        } else {
+          this.data['items'][i].children = [];
+        }
       }
     }
+  }
 
-    if (child.items != 0) {
-      child.items = [];
+  getChildren(child) {
+
+    if ( (!(child)) || (!(child.items)) ) {
+      child = {
+        items: []
+      };
     }
-    else {
+
+    if (child['items'].length !== 0) {
+      child.items = [];
+    } else {
       this.moleculeservice.getData(2).subscribe((res: any) => {
-        // child['name'] = res.name
-        child['items'] = [];
+        child['name'] = res.name
+        child.items = [];
         res.termsrelation.forEach(el => {
-          child['items'].push({ name: el.rel, children: el.terms, level: 2, loaded: true })
+          child.items.push({ name: el.rel, children: el.terms, level: 2, loaded: true });
         });
       });
     }
+
+
+
   }
 }
